9f0376c92c1426980177f11608dd64eed69e30ec
shockpast <shockpast@gmail.com>
2024-12-21 23:56:06 +0300

    feat: easier example to understand api

---
+++ examples/rainbow.lua
@@ -0,0 +1,13 @@
+pcall(require, "logi")
+
+local logi = Logitech
+
+logi.Initialize("gmod_rainbow")
+
+timer.Create("logi:Rainbow", 0.16, 0, function()
+  logi.SetLightning(HSVToColor((CurTime() * 100) % 360, 1, 1))
+end)
+
+hook.Add("ShutDown", "logi:Rainbow", function()
+  logi.Shutdown()
+end)
